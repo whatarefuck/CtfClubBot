f870aa42555485dff7add71ef7a9281d140963d3
DarkMK69 <egor2006biryukov@yandex.ru>
2025-04-09 11:05:06 +0300

    Включил планировщик

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,11 @@
 import asyncio
 import logging
 
+from tasks import restore_student_lives
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
+from pytz import timezone
 
 from aiogram import Bot, Dispatcher, types
 from handlers import (
@@ -25,14 +30,16 @@ bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher()
 
 commands = [
-    types.BotCommand(command='/start', description='Запустить бота'),
-    types.BotCommand(command='/my_tasks', description='Мои невыполненные задачи'),
-    types.BotCommand(command='/my_profile', description='Мой профиль'),
-    types.BotCommand(command='/leaderboard', description='Рейтинг участников'),
-    types.BotCommand(command='/heal', description='Исцелиться'),
-    types.BotCommand(command='/add_task', description='Отправить студентам задачу'),
-    types.BotCommand(command='/missed_deadlines', description='Посмотреть пропущенные дедлайны'),
-    types.BotCommand(command='/add_competition', description='Создать мероприятие'),
+    types.BotCommand(command="/start", description="Запустить бота"),
+    types.BotCommand(command="/my_tasks", description="Мои невыполненные задачи"),
+    types.BotCommand(command="/my_profile", description="Мой профиль"),
+    types.BotCommand(command="/leaderboard", description="Рейтинг участников"),
+    types.BotCommand(command="/heal", description="Исцелиться"),
+    types.BotCommand(command="/add_task", description="Отправить студентам задачу"),
+    types.BotCommand(
+        command="/missed_deadlines", description="Посмотреть пропущенные дедлайны"
+    ),
+    types.BotCommand(command="/add_competition", description="Создать мероприятие"),
 ]
 
 dp.include_routers(
@@ -49,9 +56,29 @@ dp.include_routers(
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    scheduler = AsyncIOScheduler()
+    # Добавляем периодическую задачу восстановления жизней
+    # Выполняется каждое 10-е число месяца в 00:00 по МСК (UTC+3)
+    scheduler.add_job(
+        restore_student_lives,
+        trigger=CronTrigger(
+            day=10,  # Каждый день
+            hour=0,   # Полночь
+            minute=0,
+            timezone=timezone('Europe/Moscow')
+        ),
+        
+        id="restore_lives",
+        name="Восстановление жизней студентов",
+        replace_existing=True,
+    )
+
+    # Запускаем планировщик
+    scheduler.start()
     await bot.set_my_commands(commands)
     # Запускаем задачу синхронизации задач
     asyncio.create_task(sync_education_tasks())  # Фоновая задача
+
     await dp.start_polling(bot)
 
 
